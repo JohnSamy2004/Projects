7473ba554bb6a486a86b65167bee2a39e6b1a368
JohnSamy2004 <jsamy2950@gmail.com>
2026-07-31 12:16:14 +0300

    Lab 1

---
+++ Programming/freecodecamp/Certified Full Stack Developer Curriculum/JavaScript/Build a Sentence Maker.js
@@ -0,0 +1,22 @@
+let adjective = "happy";
+let noun = "robot";
+let place = "park";
+let adjective2 = "shiny";
+let noun2 = "bicycle";
+let verb = "run";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// Reassign variables for second story
+adjective = "brave";
+noun = "dragon";
+place = "castle";
+adjective2 = "golden";
+noun2 = "treasure";
+verb = "flying";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
