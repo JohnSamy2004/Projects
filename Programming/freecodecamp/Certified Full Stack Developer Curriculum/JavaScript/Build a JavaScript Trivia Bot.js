990d4d4989027eb04942fa3bf92165549603f0ac
JohnSamy2004 <jsamy2950@gmail.com>
2026-07-31 12:14:13 +0300

    Lab 0

---
+++ Programming/freecodecamp/Certified Full Stack Developer Curriculum/JavaScript/Build a JavaScript Trivia Bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "awesome";
+let botLocation  = "the universe";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + (botName) + " and I live on " + botLocation + '.');
+
+console.log("My favorite programming language is " + (favoriteLanguage) + ".");
+
+let codingFact = (favoriteLanguage) + " is my favorite coding language because it has a clean, readable syntax and can be used for everything from web apps to AI!";
+
+console.log(codingFact);
+
+codingFact = (favoriteLanguage) + " is loved for its huge collection of libraries, making it easy to build games, websites, and AI applications.";
+
+console.log(codingFact);
+
+codingFact = (favoriteLanguage) + " is popular with beginners and professionals alike because its simple syntax makes code easier to read and write.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + (botName) + " from " + (botLocation) + ".")
