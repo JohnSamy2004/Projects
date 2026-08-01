d05fea828cc7cc9c0fd32242e9277b6965874558
JohnSamy2004 <jsamy2950@gmail.com>
2026-08-02 02:31:07 +0300

    Lab 8

---
+++ Programming/freecodecamp/Certified Full Stack Developer Curriculum/JavaScript/Build a Card Counting Assistant.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+function cardCounter(card){
+  switch (card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+    default:
+      break;
+  }
+
+  if (count > 0){
+    return count + ' Bet';
+  } else if (count <= 0){
+    return count + ' Hold';
+  }
+
+}
